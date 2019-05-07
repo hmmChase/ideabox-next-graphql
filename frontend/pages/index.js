ab6b9e4ba6e41a4b728d9fde165de357488ec274
hmm <hmmChase@users.noreply.github.com>
2019-05-07 10:23:31 -0700

    Update

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -5,7 +5,7 @@ import Home from '../components/Home/Home';
 const IndexPage = () => (
   <>
     <NextHead>
-      <title>next-graphql-starter-v3</title>
+      <title>ideabox-v3</title>
     </NextHead>
 
     <Home />
